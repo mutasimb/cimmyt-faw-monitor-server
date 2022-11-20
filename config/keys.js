fda3e5966611f10d10c499fb4e7c9c3a7556215a
Mutasim Billah <mutasim.wre@gmail.com>
2022-11-20 10:19:25 +0600

    mongoURI updated with srv path

--- config/keys.js
+++ config/keys.js
@@ -2,9 +2,6 @@ const {
   DB_USER,
   DB_PASSWORD,
   DB_HOST,
-  DB_HOST_00,
-  DB_HOST_01,
-  DB_HOST_02,
   DB_PORT,
   DB_NAME,
   JWT_SECRET
@@ -13,8 +10,6 @@ const {
 module.exports = {
   mongoUri: !DB_USER
     ? `mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`
-    : `mongodb://${DB_USER}:${DB_PASSWORD
-    }@${DB_HOST_00}:${DB_PORT},${DB_HOST_01}:${DB_PORT},${DB_HOST_02}:${DB_PORT}/${DB_NAME
-    }?ssl=true&replicaSet=atlas-sfvrpv-shard-0&authSource=admin&retryWrites=true&w=majority`,
+    : `mongodb+srv://${ DB_USER }:${ DB_PASSWORD }@${ DB_HOST }/${ DB_NAME }?retryWrites=true&w=majority`,
   jwtSecret: JWT_SECRET
 };
